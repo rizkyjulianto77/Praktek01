1a99af42609268ab9cbcc6310ef16ebf7c5c7d40
rizkyjulianto77 <rizkyjulianto405@gmail.com>
2023-05-23 14:20:58 +0800

    Membuat Atribut dan Behavior

--- src/praktek01/Mahasiswa.java
+++ src/praktek01/Mahasiswa.java
@@ -9,5 +9,13 @@ package praktek01;
  * @author Welcomp
  */
 public class Mahasiswa {
+    String nama;
+    String NPM;
+    double IPK;
     
+    void cetakInfo(){
+        System.out.println("Nama :"+nama);
+        System.out.println("NPM  :"+NPM);
+        System.out.println("IPK  :"+IPK);
+    }
 }
